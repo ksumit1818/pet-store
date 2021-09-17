5db163025c5a32f7a3281dc7e9644dca4cf080b8
ksumit1818 <ksumit1818@gmail.com>
2021-09-17 12:46:03 +0530

    get is working

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/pet/{petId}")
+	public ResponseEntity<String> getResp(@PathVariable Long petId) {
+		String url="https://virtserver.swaggerhub.com/asd312/temp/1.0.0/pet/1";
+		RestTemplate template = new RestTemplate();
+		ResponseEntity<String> response= template.getForEntity(url, String.class);
+		return response;
+	}
+	
+	@GetMapping("/hello")
+	public String gethello() {
+		return "Hello";
+	}
+
+}
+
---
+++ src/main/java/com/example/demo/api/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.demo.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-09-16T13:51:35.202Z")
+
+public class NotFoundException extends ApiException {
+    private int code;
+    public NotFoundException (int code, String msg) {
+        super(code, msg);
+        this.code = code;
+    }
+}
