99bcf1f8e74121ee94e52e755ed7ff9c2cc38636
ksumit1818 <ksumit1818@gmail.com>
2021-10-07 21:47:34 +0530

    junit rest template

--- src/main/java/com/example/demo/servicesImpl/PetServiceImpl.java
+++ src/main/java/com/example/demo/servicesImpl/PetServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.servicesImpl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,11 +11,19 @@ import com.example.demo.services.PetService;
 @Service
 public class PetServiceImpl implements PetService {
 
+	RestTemplate template;
+	
+	@Value("${remo.url}")
+	String url; 
 	@Override
 	public Pet getPetById(Long petId) {
-		String url = "https://virtserver.swaggerhub.com/asd312/temp/1.0.0/pet/";
-		RestTemplate template = new RestTemplate();
-		ResponseEntity<Pet> response = template.getForEntity(url+ petId, Pet.class);
+		template=new RestTemplate();
+		ResponseEntity<Pet> response=null;
+		
+		template = new RestTemplate();
+//		ResponseEntity<Pet> response = template.getForEntity(url+ petId, Pet.class);
+		response = template.getForEntity(url, Pet.class);
+//		response = template.getForEntity("https://virtserver.swaggerhub.com/asd312/temp/1.0.0/pet/1", Pet.class);
 		return response.getBody();
 	}
 
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,58 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.mock;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.servicesImpl.PetServiceImpl;
+
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Mock
+    RestTemplate restTemplate;
+    @InjectMocks
+    @Spy
+    PetServiceImpl service;
+
+    @Test
+    public void test() throws Exception {
+    	long s=1L;
+    	String res="{\r\n"
+    			+ "    \"id\": 0,\r\n"
+    			+ "    \"category\": {\r\n"
+    			+ "        \"id\": 0,\r\n"
+    			+ "        \"name\": \"string\"\r\n"
+    			+ "    },\r\n"
+    			+ "    \"name\": \"doggie\",\r\n"
+    			+ "    \"photoUrls\": [\r\n"
+    			+ "        \"string\"\r\n"
+    			+ "    ],\r\n"
+    			+ "    \"tags\": [\r\n"
+    			+ "        {\r\n"
+    			+ "            \"id\": 0,\r\n"
+    			+ "            \"name\": \"string\"\r\n"
+    			+ "        }\r\n"
+    			+ "    ],\r\n"
+    			+ "    \"status\": \"available\"\r\n"
+    			+ "}";
+    	
+        ResponseEntity<Object> res2 = new ResponseEntity<>(res,HttpStatus.OK);
+		Mockito.when(restTemplate.getForEntity(Mockito.anyString(), ArgumentMatchers.any()))
+                .thenReturn(res2);
+        service.getPetById(s);
+    }
 
 }
